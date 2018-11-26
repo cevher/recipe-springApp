a1fb481b0c24d47a081a57dd301fc0185357ca43
LAPTOP-7C5H1RJ1\Cevher <efeozden@gmail.com>
2018-11-26 15:24:25 +0300

    First Steps

---
+++ src/main/java/guru/springframework/recipespringapp/RecipeSpringappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.recipespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSpringappApplication.class, args);
+    }
+}
